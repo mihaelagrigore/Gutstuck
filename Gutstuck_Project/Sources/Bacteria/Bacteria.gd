dd6ba95787ec7be93a6896ebf461a9d450e3da45
TonySpinnou <57717935+TonySpinnou@users.noreply.github.com>
2019-12-19 11:36:44 +0100

    Evolution of energy in state chill+moving+selected

--- Gutstuck_Project/Sources/Bacteria/Bacteria.gd
+++ Gutstuck_Project/Sources/Bacteria/Bacteria.gd
@@ -4,16 +4,19 @@ var target = Vector2()
 var velocity = Vector2()
 export (int) var speed = 200
 var generation_number = 0
-var energy=50
+const initial_energy=50
+var energy = initial_energy
+var energy_after_foraging
 var time
-
-var state = 'chill' #state contains {chill, selected, moving, foraging, replication}
+var state = 'chill' #state can contain {chill, selected, moving, foraging, replicating}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	generation_number+=1
+	if generation_number >0:
+		generation_number+=1
 	pass # Replace with function body.
 
+# get.time_from general game timer # how do I do that
 
 func _input(event):
 	if state=='selected':
@@ -22,14 +25,31 @@ func _input(event):
 			target = get_global_mouse_position()
 
 func _physics_process(delta):
-	velocity = (target - position).normalized() * speed
-	#rotation = velocity.angle()
 	if state=='selected':
-	#if (target - position).length() > 5: taht was in the tuto, thought to replace it with the other if
-		move_and_slide(velocity)
+		velocity = (target - position).normalized() * speed
+		#rotation = velocity.angle()
+	#if (target - position).length() > 5: that was in the tuto, thought to replace it with the other if
+		move_and_slide(velocity) #to replace by pathfinding
 		look_at(target)
+	pass
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Evolution of energy quantity
 	if state=='chill' or state=='selected':
-		energy=func _energy_chill(time-((100-energy_after_foraging)/100))
+		energy=_energy_chill(time-((100-energy_after_foraging)/100)) #f(t), t being the time from the origin, where energy=initial_energy
+	elif state=='moving':
+		energy=_energy_moving(time-((100-energy_after_foraging)/100))
+	elif state=='foraging':
+		#energy=bite_me ?
+		energy_after_foraging=energy
+	pass
+
+func _energy_chill(t):
+	energy=100-t
+	pass
+
+func _energy_moving(t):
+	energy=90-t
 	pass
