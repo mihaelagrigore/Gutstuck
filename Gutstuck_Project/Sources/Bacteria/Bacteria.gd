67298a4ffef26c299d74486dc6d488d68b30bfe5
mihaelagrigore <grigore.mihaela@gmail.com>
2020-01-15 17:33:48 +0100

    Selected / unselected is now var 'substate' of bacteria

--- Gutstuck_Project/Sources/Bacteria/Bacteria.gd
+++ Gutstuck_Project/Sources/Bacteria/Bacteria.gd
@@ -22,12 +22,15 @@ var energy_per_bite = 10
 onready var energy_level_bar = $Energy_level_bar
 
 const STATE_CHILL = 0
-const STATE_SELECTED = 1
-const STATE_MOVING = 2
-const STATE_EATING = 3
-const STATE_FULL = 4
-const STATE_REPLICATING = 5
+const STATE_MOVING = 1
+const STATE_EATING = 2
+const STATE_FULL = 3
+const STATE_REPLICATING = 4
 
+const STATE_SELECTED = 1
+const STATE_UNSELECTED = 2
+
+var substate = STATE_UNSELECTED setget state_set, state_get
 var state = STATE_CHILL setget state_set, state_get
 var colliding_nutrients = []
 
@@ -36,6 +39,12 @@ func state_set(value):
 
 func state_get():
 	return state
+
+func substate_set(value):
+	substate = value
+
+func substate_get():
+	return substate
 
 #food_sources
 # Called when the node enters the scene tree for the first time.
@@ -46,17 +55,16 @@ func _ready():
 	pass # Replace with function body.
 
 func _input(event):
-	if state==STATE_SELECTED:
+	if substate==STATE_SELECTED:
 		$Sprite.look_at(get_global_mouse_position())
 #		if event.is_action_pressed('click'):
 #			target = get_global_mouse_position()
 
 func _physics_process(delta):
 	var velocity = Vector2()
-	if state== STATE_MOVING:
+	if state == STATE_MOVING:
 		velocity = (target - position).normalized() * speed
 		$Sprite.look_at(get_global_mouse_position())
-		#print("velocity=",velocity)
 		#rotation = velocity.angle()
 	#if (target - position).length() > 5: that was in the tuto, thought to replace it with the other if
 		move_and_slide(velocity)
