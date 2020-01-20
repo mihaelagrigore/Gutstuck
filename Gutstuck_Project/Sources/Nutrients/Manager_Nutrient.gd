d51ab69cc1f6a6d372bccc3dec1d04dfddfe7ce5
mihaelagrigore <grigore.mihaela@gmail.com>
2020-01-20 11:55:18 +0100

    Spawning Nutrients based on Timer

--- Gutstuck_Project/Sources/Nutrients/Manager_Nutrient.gd
+++ Gutstuck_Project/Sources/Nutrients/Manager_Nutrient.gd
@@ -4,36 +4,52 @@ var F_Nutrient = load("res://Sources/Nutrients/F_Nutrients/F_Nutrient.tscn")
 var P_Nutrient = load("res://Sources/Nutrients/P_Nutrients/P_Nutrient.tscn")
 
 var msk_range = 5
+var msk_population_size = 10
+
 var Spawn_Nutrient
 var spawn_position_size = 100
 
+
 func _ready():
+	$Spawn_Timer.start()
 	pass
 
 
 func personal_init():
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	var extra = 300
-
 	for i in range(msk_range):
-		var temp_nutrients = F_Nutrient.instance()
-		add_child(temp_nutrients)
+		var temp_nutrient = F_Nutrient.instance()
+		choose_spawn_location(temp_nutrient)
+		add_child(temp_nutrient)
 		
-		var temp_spawn_location_index = rng.randi_range(0, Spawn_Nutrient.get_children().size()-1)
-		var temp_spawn_location_object = Spawn_Nutrient.get_child(temp_spawn_location_index)
-		temp_nutrients.position = Vector2(rng.randf_range(temp_spawn_location_object.position.x, temp_spawn_location_object.position.x + spawn_position_size), rng.randf_range(temp_spawn_location_object.position.y, temp_spawn_location_object.position.y + spawn_position_size))
-	
 	for i in range(msk_range):
-		var temp_nutrients = P_Nutrient.instance()
-		add_child(temp_nutrients)
-		
-		var temp_spawn_location_index = rng.randi_range(0, Spawn_Nutrient.get_children().size()-1)
-		var temp_spawn_location_object = Spawn_Nutrient.get_child(temp_spawn_location_index)
-		temp_nutrients.position = Vector2(rng.randf_range(temp_spawn_location_object.position.x, temp_spawn_location_object.position.x + spawn_position_size), rng.randf_range(temp_spawn_location_object.position.y, temp_spawn_location_object.position.y + spawn_position_size))
-		
+		var temp_nutrient = P_Nutrient.instance()
+		choose_spawn_location(temp_nutrient)
+		add_child(temp_nutrient)
 	
 func _process(delta):
-	var Group_F_Nutrients=get_tree().get_nodes_in_group("F_Nutrients");
-	var Group_P_Nutrients=get_tree().get_nodes_in_group("P_Nutrients");
-	#if bacteria_state is STATE_REPLICATING, duplicate node
+	pass
+
+func choose_spawn_location(temp_nutrient: Nutrient):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var temp_spawn_location_index = rng.randi_range(0, Spawn_Nutrient.get_children().size()-1)
+	var temp_spawn_location_object = Spawn_Nutrient.get_child(temp_spawn_location_index)
+	temp_nutrient.position = Vector2(rng.randf_range(temp_spawn_location_object.position.x, temp_spawn_location_object.position.x + spawn_position_size), rng.randf_range(temp_spawn_location_object.position.y, temp_spawn_location_object.position.y + spawn_position_size))
+	
+
+func _on_Spawn_Timer_timeout() -> void:
+	if get_child_count() < msk_population_size:
+		var rng = RandomNumberGenerator.new()
+		rng.randomize()
+		var nutrient_type = rng.randi_range(0,1)
+		print(str("Random number is: ", nutrient_type))
+		
+		match nutrient_type:
+			0:
+				var temp_nutrient = F_Nutrient.instance()
+				choose_spawn_location(temp_nutrient)
+				add_child(temp_nutrient)
+			1:
+				var temp_nutrient = P_Nutrient.instance()
+				choose_spawn_location(temp_nutrient)
+				add_child(temp_nutrient)
